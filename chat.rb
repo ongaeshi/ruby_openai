d3140495342cb8adaf275f3aeccfcb3de4fa941a
ongaeshi <ongaeshi0621@gmail.com>
2023-10-05 04:02:03 +0900

    feat: ストリームに対応
    VSCode のコンソール以外だとちゃんと動かない。

--- chat.rb
+++ chat.rb
@@ -1,16 +1,27 @@
 require "ruby_openai"
 require "readline"
 
+IS_STREAM = true
+
 token = File.read(File.join(__FILE__, "../.token"))
 client = OpenAI::Client.new(access_token: token)
 
 loop do 
   input = Readline.readline("> ")
-  response = client.chat(
-    parameters: {
-        model: "gpt-3.5-turbo", # Required.
-        messages: [{ role: "user", content: input}], # Required.
-        temperature: 0.7,
-    })
-  puts response.dig("choices", 0, "message", "content")
+  parameters = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input}],
+    temperature: 0.7
+  }
+  if IS_STREAM 
+    parameters[:stream] = proc do |chunk, _bytesize|
+      print chunk.dig("choices", 0, "delta", "content")
+    end
+  end
+  response = client.chat(parameters: parameters)
+  if IS_STREAM
+    puts
+  else
+    puts response.dig("choices", 0, "message", "content")
+  end
 end
