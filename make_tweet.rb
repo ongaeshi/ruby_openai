5e39df6b497cd544bd2da416f5c681b3cbee9369
ongaeshi <ongaeshi0621@gmail.com>
2023-10-05 03:35:06 +0900

    feat: model

--- make_tweet.rb
+++ make_tweet.rb
@@ -8,6 +8,7 @@ def gpt(message, title)
   response = $client.chat(
     parameters: {
         model: "gpt-3.5-turbo",
+        # model: "gpt-4",
         messages: [{ role: "user", content: message}],
         temperature: 0.7,
     })
