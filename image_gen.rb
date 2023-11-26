21a8fd3c6be2469a5507c1c8bdbf2b8b1347104b
ongaeshi <ongaeshi0621@gmail.com>
2023-11-27 03:27:24 +0900

    feat: 自動生成

--- image_gen.rb
+++ image_gen.rb
@@ -4,8 +4,8 @@ require "open-uri"
 require "net/http"
 
 token = File.read(File.join(__FILE__, "../.token"))
-output_dir = File.join(File.dirname(__FILE__), "output")
-client = OpenAI::Client.new(access_token: token)
+$output_dir = File.join(File.dirname(__FILE__), "output")
+$client = OpenAI::Client.new(access_token: token)
 
 def download_image(url, destination_path)
   URI.open(url) do |res|
@@ -13,24 +13,43 @@ def download_image(url, destination_path)
   end
 end
 
-response = client.images.generate(parameters: {
-  model: "dall-e-2", # "dall-e-3"
-  # prompt: 'Use pixel art to represent the single weapon in a "Halloween" themed game as "fireball". Create the background with black for transparency. Square. Use larger pixel dots.', 
-  # prompt: 'Use pixel art to represent the single enemy in a "Halloween" themed game as "cute ghost". Create the background with black for transparency. Square. Use larger pixel dots.', 
-  # prompt: 'Use pixel art to represent the single hero in a "Halloween" themed game as "cute small knight". Create the background with black. Square. Use larger pixel dots.', 
-  # prompt: 'Use pixel art to represent the single hero in a "Cyberpunk" themed game as "cute small knight". Create the background with black. Square. Use larger pixel dots.', 
-  # prompt: 'Use pixel art to represent the single hero in a "Cyberpunk" themed game as "young boy". Create the background with black. Square. From side to right. Use larger pixel dots.', 
-  # prompt: 'Use pixel art to represent the single hero in a "Cyberpunk" themed game as "boy with gun". Create the background with black. Square. From side to right. Use larger pixel dots.', 
-  prompt: 'Use pixel art to represent the single hero in a "Age of Exploration" themed game as "pirate girl". Create the background with black. Square. From side to right. Use larger pixel dots.', 
-  size: "256x256", # "1024x1024", 
-  n: 1
-  })
-# p response
-url = response.dig("data", 0, "url")
-# puts url
+def generate_image(model, prompt, size)
+  # https://platform.openai.com/docs/api-reference/images/create
+  response = $client.images.generate(parameters: {
+    # "dall-e-2" OR "dall-e-3"
+    model: model,
+    # dall-e-2: 1000 characters
+    # dall-e-3: 4000 characters
+    prompt: prompt,
+    # dall-e-2: 256x256, 512x512, or 1024x1024
+    # dall-e-3: 1024x1024, 1024x1792 or 1792x1024
+    size: size,
+    })
+end
 
-timestamp = Time.now.strftime("%Y%m%d%H%M%S")
-name = "player1_#{timestamp}.png"
-path = File.join(output_dir, name)
-puts "Save to #{path}"
-download_image(url, path)
+def generate_common_game_asset(thema, data)
+  timestamp = Time.now.strftime("%Y%m%d%H%M%S")
+  data.each do |d|
+    response = generate_image(
+      "dall-e-2",
+      "Use pixel art to represent the single #{d[:kind]} in a '#{thema}' themed game as '#{d[:description]}'. Create the background with black. Square. From side to right. Use larger pixel dots.",
+      "256x256"
+    )
+    url = response.dig("data", 0, "url")
+    name = "#{d[:kind]}_#{timestamp}.png"
+    path = File.join($output_dir, name)
+    puts "Save to #{path}"
+    download_image(url, path)
+  end
+end
+
+generate_common_game_asset(
+  "Halloween",
+  [
+    {kind: "player1", description: "Cute Ghost"},
+    {kind: "enemy1", description: "Scary Pumpkin"},
+    {kind: "weapon1", description: "Fireball"},
+    {kind: "item1", description: "Lucky Coin"},
+    {kind: "tile1", description: "Brick tile"},
+  ]
+)
